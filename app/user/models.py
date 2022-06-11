382648306b57920527b0b7184c7938c8ae14ce38
jwywoo <jwywoo26@gmail.com>
2022-06-10 21:19:32 -0400

    17th commit: creating forms.py and models.py for user

--- app/user/models.py
+++ app/user/models.py
@@ -3,18 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 # AbstractUser?
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    favorites = []
+# class User(models.Model):
+#     username = models.CharField(max_length=100)
+#     password = models.CharField(max_length=100)
+#     favorites = []
 
-# class User(AbstractUser):
-    # pass
-    # favorite_foods = models.ManyToManyField(
-    #     'self',
-    #     through='Relations',
-    #     symmetrical=False,
-    #     blank=True,
-    #     related_name='from_relation_users',
-    #     related_query_name='from_relation_user',
-    # )
+class User(AbstractUser):
+    email = models.EmailField(blank=True)
+    food_list = models.ManyToManyField(
+        related_name='like_food',
+        blank=True,
+    )
