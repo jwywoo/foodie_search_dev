d437ca86a6ad1850eff4706f1e11080ad2e439f2
Paul <paulabijaber@gmail.com>
2022-06-20 18:16:16 -0400

    Ok now user testing is done

--- app/user/tests.py
+++ app/user/tests.py
@@ -1,4 +1,3 @@
-from multiprocessing.connection import Client
 from django.test import TestCase
 from food.models import *
 from .models import User
@@ -15,14 +14,6 @@ class TestViews(TestCase):
     #####################################################
     #~~~~~~~~~~~~~ TESTS FOR USER_SIGNUP ~~~~~~~~~~~~~~ #
     #####################################################
-
-    def test_user_signup_GetRequest(self):
-
-        response = self.client.get('/user/signUp/')
-
-        self.assertEquals(response.status_code, 200)
-        self.assertContains(response, "Signup Here!")
-
 
     def test_user_signup_Valid(self):
 
@@ -55,23 +46,90 @@ class TestViews(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertEquals(len(User.objects.all()), 0)
 
+
+    def test_user_signup_GetRequest(self):
+
+        response = self.client.get('/user/signUp/')
+
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "Signup Here!")
+
+    #####################################################
+    #~~~~~~~~~~~~~ TESTS FOR USER_LOGIN  ~~~~~~~~~~~~~~ #
+    #####################################################
+
+    def test_user_login_Valid(self):
+
+        # Create User instance in database
+        user = User.objects.create_user(username='paul', email='paulabijaber@gmail.com', password='password')
+        response = self.client.login(username='paul',password='password')
+
+        # Check if user is logged in and it exists
+        self.assertTrue(response)
+        self.assertEqual(User.objects.get(pk=1), user)
+
+
+    def test_user_login_NotValid(self):
+
+        user = User.objects.create_user(username='paul', email='paulabijaber@gmail.com', password='password')
+        response = self.client.login(username='paul',password='pawdas')
+
+        # Check that login failed
+        self.assertFalse(response)
+
+        # Check that failed login prints correct message
+        response = self.client.post('/user/login/', data={'username':'padul','password':'password'})
+        self.assertContains(response, 'Wrong password/username')
+
+
+    def test_user_login_GetRequest(self):
+
+        response = self.client.get('/user/login/')
+
+        # Website exists 
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, 'Welcome Back!')
+    
+
+    # INTEGRATION TESTING SignUp -> SignIn
+    def test_user_signupLogin(self):
+
+        # Mock input for signup
+        data = {
+            'username': 'paul',
+            'email' : 'paulabijaber@gmail.com',
+            'password': 'password',
+            'password2': 'password'
+        }
+        response = self.client.post('/user/signUp/', data=data)
+
+        # Check if site exists and Databse contains my info
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(User.objects.get(pk=1).username, 'paul')
+
+        # Check user is logged in
+        response = self.client.post('/user/login/', data={'username':'paul','password':'password'})
+        self.assertIn('_auth_user_id', self.client.session)
+
+
     #####################################################
     #~~~~~~~~~~~~~ TESTS FOR USER_LOGOUT ~~~~~~~~~~~~~~ #
     #####################################################
 
+    # INTEGRATION TESTING SignUp -> SignIn -> SignOut
     def test_user_logout(self):
 
-        User.objects.create_user(username='paul', email='paulabijaber@gmail.com', password='password')
+        # Create User instance in database
+        user = User.objects.create_user(username='paul', email='paulabijaber@gmail.com', password='password')
+        response = self.client.login(username='paul',password='password')
 
-        data = {
-            'username': 'paul',
-            'password': 'password'
-        }
+        # Check if user is logged in and it exists
+        self.assertTrue(response)
+        self.assertEqual(User.objects.get(pk=1), user)
 
-        c = Client
-        response = c.get('/user/login/', data=data)
-        response = c.get('/user/logout/')
-        
-        self.assertEquals(response.status_code, 302)
-
+        # Logout and check we are logged out
+        response = self.client.get('/user/logout/')
+        self.assertEquals(response.status_code, 200)
+        self.assertNotContains(response, 'Logout')
+        self.assertContains(response, 'Signup')
 
